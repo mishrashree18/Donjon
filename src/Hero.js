c1bc2937e28540786c94cdd69c2d00a92a8d68b9
mishrashree18 <mishrashree18@gmail.com>
2021-09-14 11:53:06 +0530

    :memo: Mobile view update

--- src/Hero.js
+++ src/Hero.js
@@ -7,15 +7,15 @@ const Hero = ({ notes, handleAddNote, handleDeleteNote, handleSearchNote }) => {
   return (
     <section className="hero">
       <nav className="flex flex-col space-y-2">
-        <h1 className="text-green-300">Donjon</h1>
-        <h4 className="text-gray-500 font-medium">Dunjon is the best place to jot down quick thoughts or to save notes.</h4>
+        <h1 className="text-green-300 text-center">Donjon</h1>
+        <h4 className="text-gray-500 font-medium text-sm text-center">Dunjon is the best place to jot down quick thoughts or to save notes.</h4>
       </nav>
 
       <div className="bar text-green-300">
         <Search handleSearchNote={handleSearchNote} />
       </div>
-      <body className="justify-center items-center">
-        <div className="notes-list">
+      <body className="justify-center items-center flex">
+        <div className="notes-list justify-center items-center mr-5 mt-3">
           {notes.map((note) => (
             <Note
               id={note.id}
